90278812910bc8b84a068862836acba2adc9e7e3
suragch <studymongolian@gmail.com>
2018-12-14 09:28:30 -0800

    returned state to initial tutorial setup

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
 void main() {
-  debugPaintSizeEnabled = true;
+  //debugPaintSizeEnabled = true;
   runApp(MyApp());
 }
 
@@ -25,9 +25,9 @@ class MyApp extends StatelessWidget {
 
 // initial setup
 
-//Widget myLayoutWidget() {
-//  return Text("Hello world!");
-//}
+Widget myLayoutWidget() {
+  return Text("Hello world!");
+}
 
 // padding
 
@@ -199,77 +199,77 @@ class MyApp extends StatelessWidget {
 //  );
 //}
 
-Widget myLayoutWidget() {
-
-  // wrap everything in a purple container
-  return Container(
-    margin: EdgeInsets.all(16.0),
-    padding: EdgeInsets.all(16.0),
-    decoration: BoxDecoration(
-      color: Colors.purple[900],
-      border: Border.all(),
-      borderRadius: BorderRadius.all(Radius.circular(3.0)),
-    ),
-
-    // column of three rows
-    child: Column(
-
-      // this makes the column height hug its content
-      mainAxisSize: MainAxisSize.min,
-      children: [
-
-        // first row
-        Row(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(right: 8.0),
-              child: Icon(Icons.favorite,
-                color: Colors.green,
-              ),
-            ),
-            Text(
-              'BEAMS',
-              style: TextStyle(
-                color: Colors.white,
-              ),
-            ),
-          ],
-        ),
-
-        // second row (single item)
-        Padding(
-          padding: EdgeInsets.symmetric(
-            vertical: 16.0,
-            horizontal: 0,
-          ),
-          child: Text('Send programmable push notifications to iOS and Android devices with delivery and open rate tracking built in.',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-
-        // third row
-        Row(
-          children: [
-            Text('EXPLORE BEAMS',
-              style: TextStyle(
-                color: Colors.green,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(left: 8.0),
-              child: Icon(Icons.arrow_forward,
-                color: Colors.green,
-              ),
-            ),
-          ],
-        ),
-
-      ],
-    ),
-  );
-}
+//Widget myLayoutWidget() {
+//
+//  // wrap everything in a purple container
+//  return Container(
+//    margin: EdgeInsets.all(16.0),
+//    padding: EdgeInsets.all(16.0),
+//    decoration: BoxDecoration(
+//      color: Colors.purple[900],
+//      border: Border.all(),
+//      borderRadius: BorderRadius.all(Radius.circular(3.0)),
+//    ),
+//
+//    // column of three rows
+//    child: Column(
+//
+//      // this makes the column height hug its content
+//      mainAxisSize: MainAxisSize.min,
+//      children: [
+//
+//        // first row
+//        Row(
+//          children: [
+//            Padding(
+//              padding: EdgeInsets.only(right: 8.0),
+//              child: Icon(Icons.favorite,
+//                color: Colors.green,
+//              ),
+//            ),
+//            Text(
+//              'BEAMS',
+//              style: TextStyle(
+//                color: Colors.white,
+//              ),
+//            ),
+//          ],
+//        ),
+//
+//        // second row (single item)
+//        Padding(
+//          padding: EdgeInsets.symmetric(
+//            vertical: 16.0,
+//            horizontal: 0,
+//          ),
+//          child: Text('Send programmable push notifications to iOS and Android devices with delivery and open rate tracking built in.',
+//            style: TextStyle(
+//              color: Colors.white,
+//            ),
+//          ),
+//        ),
+//
+//        // third row
+//        Row(
+//          children: [
+//            Text('EXPLORE BEAMS',
+//              style: TextStyle(
+//                color: Colors.green,
+//              ),
+//            ),
+//            Padding(
+//              padding: EdgeInsets.only(left: 8.0),
+//              child: Icon(Icons.arrow_forward,
+//                color: Colors.green,
+//              ),
+//            ),
+//          ],
+//        ),
+//
+//      ],
+//    ),
+//  );
+//}
 
 // first row as a function
 
